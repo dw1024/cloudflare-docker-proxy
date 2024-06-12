0f788d09f9b134cc385b95025a203e70b51fea63
dw1024 <38034033+dw1024@users.noreply.github.com>
2024-06-12 09:35:53 +0800

    Update index.js
    修改域名

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.foulai.com": "https://registry-1.docker.io",
+  "quay.foulai.com": "https://quay.io",
+  "gcr.foulai.com": "https://gcr.io",
+  "k8s-gcr.foulai.com": "https://k8s.gcr.io",
+  "k8s.foulai.com": "https://registry.k8s.io",
+  "ghcr.foulai.com": "https://ghcr.io",
+  "cloudsmith.foulai.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
